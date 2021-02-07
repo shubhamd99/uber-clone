9279ec91cc00706b1d334bc1aa91d3728b5a10db
Shubham Dhage <shubhamd@rigbot.com>
2021-02-07 10:52:03 +0530

    fix height of bottom container in Uber Types component

--- src/components/UberTypes/styles.js
+++ src/components/UberTypes/styles.js
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     bottomContainer: {
-        height: 140,
+        height: 145,
         backgroundColor: Colors.white,
         paddingBottom: 15,
     }
