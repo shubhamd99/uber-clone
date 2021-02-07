30cc09ce5461b04a94945527c31c4cc52d587ee8
Shubham Dhage <shubhamd@rigbot.com>
2021-02-07 16:55:40 +0530

    add react native geolocation package and ask for permission function

--- App.js
+++ App.js
@@ -6,13 +6,48 @@
  * @flow strict-local
  */
 
-import React from "react";
-import { StatusBar } from "react-native";
+import React, { useEffect } from "react";
+import { StatusBar, PermissionsAndroid, Platform } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import TripsScreen from "./src/screens/TripsScreen";
 import DestinationScreen from "./src/screens/DestinationScreen";
+import Geolocation from "@react-native-community/geolocation";
 
 const App = () => {
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Uber App Location Permission",
+          message:
+            "Uber App needs access to your location " +
+            "so you can take awesome rides.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location");
+      } else {
+        console.log("Location permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+        requestLocationPermission();
+    } else {
+      // IOS
+      Geolocation.requestAuthorization();
+    }
+  }, []); // Empty array means it will run only on component mount
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
--- README.md
+++ README.md
@@ -12,3 +12,9 @@
 1. yarn add react-native-vector-icons
 2. IOS - Add UIAppFonts in Info.plist file and then do `npx pod install` in ios/ directory
 3. Android - Add this `apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"` in android/app/build.gradle
+
+* Get Current Position - https://www.npmjs.com/package/@react-native-community/geolocation
+1. yarn add @react-native-community/geolocation
+2. IOS - Add NSLocationAlwaysAndWhenInUseUsageDescription in info.plist
+3. Android - Add android.permission.ACCESS_FINE_LOCATION in AndroidManifest.xml
+4. cd ios && pod install
